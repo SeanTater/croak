f9275a1328e85f73fa7888d594854574a8a07689
Sean Gallagher <stgallag@gmail.com>
2022-06-07 18:59:40 -0400

    Start moving from text to audio

--- croak/model.py
+++ croak/model.py
@@ -1,31 +1,97 @@
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
-from tensorflow.keras.layers import TextVectorization
 import numpy as np
-import os
-import re
-import string
 import random
-from glob import glob
-from soundfile import 
+from pathlib import Path
+import soundfile as sf
 
+class AudioDataset:
+    """ Open a folder full of OPUS audio files to use as training data for a neural network.
+        This works by reading the audio, splitting it into frames, and presenting several frames at once as an example.
+        The target of the network is to predict the next frame.
+    """
+    def __init__(self, root: Path, frame_width:int=128, chain_length:int=128):
+        """ Open a new dataset.
 
-def causal_attention_mask(batch_size, n_dest, n_src, dtype):
-    """
-    Mask the upper half of the dot product matrix in self attention.
-    This prevents flow of information from future tokens to current token.
-    1's in the lower triangle, counting from the lower right corner.
-    """
-    i = tf.range(n_dest)[:, None]
-    j = tf.range(n_src)
-    m = i >= j - n_src + n_dest
-    mask = tf.cast(m, dtype)
-    mask = tf.reshape(mask, [1, n_dest, n_src])
-    mult = tf.concat(
-        [tf.expand_dims(batch_size, -1), tf.constant([1, 1], dtype=tf.int32)], 0
-    )
-    return tf.tile(mask, mult)
+        Parameters
+        ==========
+        root: Path
+            The directory the OPUS files are stored in.
+        frame_width: int
+            The width of each frame in samples.
+        chain_length: int
+            The number of frames to use as input to the network.
+        """
+        self.root = root
+        self.files = list(self.root.glob("**/*.opus"))
+        self.frame_width = frame_width
+        self.chain_length = chain_length
+        self.buffer = np.zeros(0)
+        self.cursor=0
+
+    def generate_music_batch(self, batch_size:int=1):
+        """ Generate a minibatch for training.
+
+        Parameters
+        ==========
+        batch_size: int
+            The number of examples to generate.
+        """
+        start, end = self.cursor, self.cursor + (self.frame_width * self.chain_length * batch_size)
+        # Refill the buffer if we're at the end
+        if self.buffer.size <= end:
+            filename = random.choice(self.files)
+            self.buffer = sf.read(filename)[0] # a numpy array of shape (n_samples, n_channels)
+            self.cursor = 0
+            assert self.buffer.size > end, "Buffer is too small to generate a batch of size {}".format(batch_size)
+        # Extract the data
+        data = self.buffer[start:end]
+        # Reshape it to a batch of examples
+        data = data.reshape((batch_size, self.chain_length, self.frame_width))
+        # Split it into the inputs and output along the chain
+        inputs = data[:, :-1]
+        outputs = data[:, 1:]
+        # Skip forward
+        self.cursor = end
+        # Return the data
+        return (inputs, outputs)
+
+class MusicModelWrapper:
+    def __init__(self,
+            window_count:int=100,
+            embed_dim:int=128,
+            num_heads:int=2,
+            feed_forward_dim:int=128,
+            window_size:int=128,
+            num_layers:int=2,
+        ):
+        self.window_count = window_count
+        self.window_size = window_size
+        self.embed_dim = embed_dim
+        self.num_heads = num_heads
+        self.feed_forward_dim = feed_forward_dim
+        self.num_layers = num_layers
+        self.model = self._create_model()
+    
+    def _create_model(self) -> keras.Model:
+        inputs = layers.Input(shape=(self.window_count,), dtype=tf.int32)
+        x = PositionEmbedding(self.window_count, self.embed_dim)(inputs)
+        for _ in range(self.num_layers):
+            x = TransformerBlock(self.embed_dim, self.num_heads, self.feed_forward_dim)(x)
+        outputs = layers.Dense(self.window_size)(x)
+        model = keras.Model(inputs=inputs, outputs=[outputs, x])
+        model.compile(
+            "adam", loss="mse", metrics=["mse"]
+        )  # No loss and optimization based on word embeddings from transformer block
+        return model
+    
+    def fit(self, dataset:AudioDataset, epochs:int=100, batch_size:int=32):
+        self.model.fit(
+            dataset,
+            epochs=epochs,
+            batch_size=batch_size
+        )
 
 
 class TransformerBlock(layers.Layer):
@@ -44,7 +110,7 @@ class TransformerBlock(layers.Layer):
         input_shape = tf.shape(inputs)
         batch_size = input_shape[0]
         seq_len = input_shape[1]
-        causal_mask = causal_attention_mask(batch_size, seq_len, seq_len, tf.bool)
+        causal_mask = self.causal_attention_mask(batch_size, seq_len, seq_len, tf.bool)
         attention_output = self.att(inputs, inputs, attention_mask=causal_mask)
         attention_output = self.dropout1(attention_output)
         out1 = self.layernorm1(inputs + attention_output)
@@ -52,73 +118,34 @@ class TransformerBlock(layers.Layer):
         ffn_output = self.dropout2(ffn_output)
         return self.layernorm2(out1 + ffn_output)
 
-class TokenAndPositionEmbedding(layers.Layer):
-    def __init__(self, maxlen, vocab_size, embed_dim):
-        super(TokenAndPositionEmbedding, self).__init__()
-        self.token_emb = layers.Embedding(input_dim=vocab_size, output_dim=embed_dim)
-        self.pos_emb = layers.Embedding(input_dim=maxlen, output_dim=embed_dim)
 
-    def call(self, x):
-        maxlen = tf.shape(x)[-1]
-        positions = tf.range(start=0, limit=maxlen, delta=1)
-        positions = self.pos_emb(positions)
-        x = self.token_emb(x)
-        return x + positions
+    @staticmethod
+    def causal_attention_mask(batch_size, n_dest, n_src, dtype):
+        """
+        Mask the upper half of the dot product matrix in self attention.
+        This prevents flow of information from future tokens to current token.
+        1's in the lower triangle, counting from the lower right corner.
+        """
+        i = tf.range(n_dest)[:, None]
+        j = tf.range(n_src)
+        m = i >= j - n_src + n_dest
+        mask = tf.cast(m, dtype)
+        mask = tf.reshape(mask, [1, n_dest, n_src])
+        mult = tf.concat(
+            [tf.expand_dims(batch_size, -1), tf.constant([1, 1], dtype=tf.int32)], 0
+        )
+        return tf.tile(mask, mult)
 
 
 class PositionEmbedding(layers.Layer):
-    def __init__(self, maxlen, vocab_size, embed_dim):
+    def __init__(self, maxlen, embed_dim):
         super(PositionEmbedding, self).__init__()
         self.pos_emb = layers.Embedding(input_dim=maxlen, output_dim=embed_dim)
 
     def call(self, x):
         maxlen = tf.shape(x)[-1]
         positions = tf.range(start=0, limit=maxlen, delta=1)
-        return self.pos_emb(positions)
-
-vocab_size = 96  # Only consider the top 96 characters
-maxlen = 100  # Max sequence size
-embed_dim = 128  # Embedding size for each token
-num_heads = 2  # Number of attention heads
-feed_forward_dim = 128  # Hidden layer size in feed forward network inside transformer
-subsample_slices = 17 # How many characters to skip forward in each article for each slice
-
-
-def create_model():
-    inputs = layers.Input(shape=(maxlen,), dtype=tf.int32)
-    embedding_layer = TokenAndPositionEmbedding(maxlen, vocab_size, embed_dim)
-    x = embedding_layer(inputs)
-    transformer_block = TransformerBlock(embed_dim, num_heads, feed_forward_dim)
-    x = transformer_block(x)
-    outputs = layers.Dense(vocab_size)(x)
-    model = keras.Model(inputs=inputs, outputs=[outputs, x])
-    loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-    model.compile(
-        "adam", loss=[loss_fn, None],
-    )  # No loss and optimization based on word embeddings from transformer block
-    return model
-
-batch_size = 128
-
-def generate_music_batch(size=1):
-    
-
-
-def prepare_lm_inputs_labels(text):
-    """
-    Shift word sequences by 1 position so that the target for position (i) is
-    word at position (i+1). The model will use all words up till position (i)
-    to predict the next word.
-    """
-    text = tf.expand_dims(text, -1)
-    tokenized_sentences = vectorize_layer(text)
-    x = tokenized_sentences[:, :-1]
-    y = tokenized_sentences[:, 1:]
-    return x, y
-
-
-text_ds = text_ds.map(prepare_lm_inputs_labels)
-text_ds = text_ds.prefetch(tf.data.AUTOTUNE)
+        return x + self.pos_emb(positions)
 
 
 class TextGenerator(keras.callbacks.Callback):
@@ -161,11 +188,11 @@ class TextGenerator(keras.callbacks.Callback):
         num_tokens_generated = 0
         tokens_generated = []
         while num_tokens_generated <= self.max_tokens:
-            pad_len = maxlen - len(start_tokens)
+            pad_len = self.window_count - len(start_tokens)
             sample_index = len(start_tokens) - 1
             if pad_len < 0:
-                x = start_tokens[:maxlen]
-                sample_index = maxlen - 1
+                x = start_tokens[:self.window_count]
+                sample_index = self.window_count - 1
             elif pad_len > 0:
                 x = start_tokens + [0] * pad_len
             else:
@@ -180,6 +207,29 @@ class TextGenerator(keras.callbacks.Callback):
             [self.detokenize(_) for _ in self.start_tokens + tokens_generated]
         )
         print(f"generated text:\n{txt}\n")
+
+
+if __name__ == "__main__":
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument("--batch_size", type=int, default=32)
+    parser.add_argument("--epochs", type=int, default=100)
+    parser.add_argument("--window-count", type=int, default=128)
+    parser.add_argument("--window-size", type=int, default=128)
+    parser.add_argument("--embed-dim", type=int, default=128)
+    parser.add_argument("--num-heads", type=int, default=8)
+    parser.add_argument("--num-layers", type=int, default=2)
+    parser.add_argument("--dropout", type=float, default=0.1)
+    args = parser.parse_args()
+    model = MusicModelWrapper(
+        embed_dim=args.embed_dim,
+        num_heads=args.num_heads,
+        feed_forward_dim=args.embed_dim, # Set the same for now
+        dropout_rate=args.dropout_rate,
+        num_layers=args.num_layers,
+        window_count=args.window_count,
+        window_size=args.window_size,
+    )
 
 
 # Tokenize starting prompt
